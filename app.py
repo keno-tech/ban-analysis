f94df3026df10945ba4dc1205b395d146f657670
keno-tech <techin.keno2@gmail.com>
2025-02-07 19:04:50 +0700

    added req

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import requests
 import pprint
+import os
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -183,4 +184,5 @@ def index():
                          hero_images=HERO_IMAGES)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
